25f2d07ea00cfe38a2ebd6e14db887135a36457d
Denis Uglov <denisuglov1981@gmail.com>
2022-01-12 16:10:17 +0600

    add: solution of the task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    let dig = Math.abs(n);
+    let rev = 0;
+    while (dig > 0) {
+      rev = rev * 10 + (dig % 10);
+      dig = Math.floor(dig / 10);
+    }
+    return rev;
 }
+
